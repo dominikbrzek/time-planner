320d04d5ee52d33829c8fa5f1989ef798bce862c
DominikBrzek <finansdobr@gmail.com>
2024-07-09 21:49:10 +0200

    Initial commit

---
+++ backend/time-planner/src/test/java/time_planner/TimePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package time_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimePlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
